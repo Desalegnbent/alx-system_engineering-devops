2ff1b7923dd812c3c3dc78946f771042512ddb83
Desalegnbent <desalegnbenti@gmail.com>
2024-05-10 11:01:15 -0700

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# done by me
+# done by desalegn benti
 
-puts ARGV[0].scan(/\[from:(.*?\] \[to:(.*?\] \[flags:(.*?\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
